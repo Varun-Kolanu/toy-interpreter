303e8ccefc45af261ff45c546196a604e1f9c722
Varun-Kolanu <kolanuvarun739@gmail.com>
2024-12-12 10:05:19 +0530

    ast: add visitor pattern

--- scripts/generate_ast.cjs
+++ scripts/generate_ast.cjs
@@ -1,8 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 
-
-
 function generateAst() {
     const filePath = path.join(__dirname, '../src/ast.rs');
     const base_name = 'Expr'
@@ -31,7 +29,47 @@ pub enum ${base_name} {
         child += "\t},\n";
         children.push(child);
     })
-    children.push("}");
+    children.push("}\n\n");
+
+    // Visitor
+    children.push("pub trait Visitor<T> {\n");
+    Object.keys(expression_types).forEach(key => {
+        let child = `\tfn visit_${key.toLowerCase()}_${base_name.toLowerCase()}(&mut self,`
+        expression_types[key].split(",").forEach(field => {
+            field = field.trim();
+            const k = field.split(" ")[0];
+            let v = field.split(" ")[1];
+            if (v == "base") v = `${base_name}`;
+            child += ` ${k}: &${v},`;
+        })
+        child += ") -> T;\n";
+        children.push(child);
+    })
+    children.push("}\n\n");
+
+    // Accept
+    children.push(`impl ${base_name} {
+    pub fn accept<T>(&self, visitor: &mut dyn Visitor<T>) -> T {
+        match self {\n`);
+
+    Object.keys(expression_types).forEach(key => {
+        let child = `\t\t\t${base_name}::${key} {`
+        expression_types[key].split(",").forEach(field => {
+            field = field.trim();
+            const k = field.split(" ")[0];
+            child += ` ${k},`;
+        })
+        child += `} => visitor.visit_${key.toLowerCase()}_${base_name.toLowerCase()}(`;
+        expression_types[key].split(",").forEach(field => {
+            field = field.trim();
+            const k = field.split(" ")[0];
+            child += `${k}, `;
+        })
+        child += "),\n"
+        children.push(child);
+    })
+    children.push("\t\t}\n\t}\n}\n");
+
 
     fileContent += children.join("");
 
